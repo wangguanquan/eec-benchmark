7c1a4da44148df642e36e3f5b49ad7dec186dd50
guanquan.wang <guanquan.wang@yandex.com>
2023-08-29 08:42:04 +0800

    update

--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env sh
+
 javac -cp .:./eec-lib/eec-0.5.11-SNAPSHOT.jar:./easy-lib/easyexcel-core-3.3.2.jar LargeData.java
 javac -encoding utf-8 RandomDataProvider.java
 
@@ -9,11 +11,11 @@ javac -encoding utf-8 -cp .:./eec-lib/eec-0.5.11-SNAPSHOT.jar EecWriteBenchmarkT
 javac -encoding utf-8 -cp .:./fast-lib/commons-compress-1.23.0.jar:./fast-lib/stax2-api-4.2.jar:./fast-lib/aalto-xml-1.3.2.jar:./fast-lib/fastexcel-0.15.6.jar FastWriteBenchmarkTest.java
 javac -encoding utf-8 -cp .:./easy-lib/easyexcel-core-3.3.2.jar EasyWriteBenchmarkTest.java
 
-@rem do Writer
+# do Writer
 sh ./writer.sh
 
-@rem do Reader
+# do Reader
 sh ./reader.sh
 
-@rem Cycle reading 10 times
-@rem for /l %%i in (1,1,10) do inner1
+# Cycle reading 10 times
+# for /l %%i in (1,1,10) do inner1
